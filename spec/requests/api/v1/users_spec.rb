b9c661820c9e92aae43c872d8200ebdc881ca89f
Arthur Moura <arthurjm95@gmail.com>
2017-10-30 16:33:45 +0000

    adicionando erro para verificar em outra maquina

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,66 @@
+require 'rails_helper'
+
+RSpec.describe 'Users API', type: :request do
+  let!(:user){ create(:user) }
+  let(:user_id) { user.id }
+
+  before { host! 'api.hubcoapp.dev'}
+
+  describe 'GET /users/:id' do
+    before do
+      headers = { 'Accept'  => 'application/vnd.emam.v1'}
+      get "/users/#{user_id}", params: {}, headers: headers
+    end
+
+    context 'when the user exists' do
+      it 'returns the user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['id']).to eq(user_id)
+      end
+
+      it 'returns status 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+    context 'returns status does not exist' do
+      let(:user_id) { 99999 }
+
+      it 'returns status code 400' do
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+ describe "POST /users" do
+    before do
+      headers = { "Accept" => "application/vnd.taskmanager.v1", 'Content-Type' => 'application/json' }
+      post '/users', params: { user: user_params }, headers: headers
+    end
+
+    context 'when the request params are valid' do
+      let(:user_params) { attributes_for(:user) }
+
+      it 'returns status code 201' do
+        expect(response).to have_http_status(201)
+      end
+
+      it 'returns the json data for the created user' do
+        user_response = JSON.parse(response.body)
+        expect(user_response['email']).to eq(user_params[:email])
+      end
+    end
+
+    context 'when the request params are invalid' do
+      let(:user_params) { attributes_for(:user, email: 'invalid.email@') }
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+
+      it 'returns the json data for the errors' do
+        user_response = JSON.parse(response.body)
+        expect(user_response).to have_key('errors')
+      end
+    end
+  end
+end
