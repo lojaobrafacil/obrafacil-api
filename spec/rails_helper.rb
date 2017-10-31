c3a5237f9e4d1b6c01bd9e6c4f76c10a8fb44cc6
Arthur Moura <arthurjm95@gmail.com>
2017-10-31 15:54:26 +0000

    adicionando authenticate_with_token! para usuarios

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,7 +42,7 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
