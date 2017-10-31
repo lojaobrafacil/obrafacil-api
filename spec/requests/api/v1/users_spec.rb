1ff23393e58254d12684c901d6641436d0dfaac2
Arthur Moura <arthurjm95@gmail.com>
2017-10-31 16:02:47 +0000

    adicionando autenticação para usuario

--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe 'Users API', type: :request do
   let(:headers) do
     {
       'Accept'  => 'application/vnd.emam.v1',
-      'Content-type' => Mime[:json].to_s
+      'Content-type' => Mime[:json].to_s,
+      'Authorization' => user.auth_token
     }
   end
 
