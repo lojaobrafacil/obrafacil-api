a5cdcc4d889cbf6a8e522f48d5e6287661be8781
Arthur Moura <arthurjm95@gmail.com>
2017-11-23 13:04:58 +0000

    unicorn

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,13 +1,24 @@
-root = "/var/api/emam/current"
-working_directory root
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/var/www/my_app"
 
-pid "#{root}/tmp/pids/unicorn.pid"
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/var/www/my_app/pids/unicorn.pid"
 
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/var/www/my_app/log/unicorn.log"
+stdout_path "/var/www/my_app/log/unicorn.log"
 
-worker_processes 4
+# Unicorn socket
+listen "/tmp/unicorn.[app name].sock"
+listen "/tmp/unicorn.myapp.sock"
+
+# Number of processes
+# worker_processes 4
+worker_processes 2
+
+# Time-out
 timeout 30
-preload_app true
-
-listen '/tmp/emam.sock', backlog: 64
