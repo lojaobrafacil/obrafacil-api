c134b2e4f4edc8024f255493becbe8c65e24d717
Arthur Moura <arthurjm95@gmail.com>
2017-11-27 13:10:25 +0000

    remove swag das rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   	namespace :api, default: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
   		namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
