bb64f28f0ba670cff23f61a6ff535bc735dee616
Arthur Moura <arthurjm95@gmail.com>
2018-02-27 16:31:56 +0000

    test

--- app/serializers/api/v1/partner_serializer.rb
+++ app/serializers/api/v1/partner_serializer.rb
@@ -4,6 +4,4 @@ class Api::V1::PartnerSerializer < ActiveModel::Serializer
   :limit, :origin, :percent, :agency, :account, :favored, :bank, :billing_type, :user, 
   :phones, :emails, :updated_at, :created_at
 
-  has_many :addresses
-
 end
