1eb09645b548e86ecd724a19493ee1b113288668
Arthur Moura <arthurjm95@gmail.com>
2018-02-27 18:14:15 +0000

    test

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -3,9 +3,7 @@ class Api::V1::ProductSerializer < ActiveModel::Serializer
   :cost, :tax_industrialized_products, :profit_margin, :aliquot_merchandise_tax,
   :bar_code, :tax_substitution, :tax_reduction, :discount, :weight, :height, :width,
   :length, :color, :code_tax_substitution_specification, :kind, :active, :category_id,
-  :sub_category_id, :unit_id, :provider_id, :sku, :sku_xml, :updated_at, :created_at
-
-  has_many :company_products, serializer: CompanyProductSerializer
+  :sub_category_id, :unit_id, :provider_id, :sku, :sku_xml, :company_products, :updated_at, :created_at
 
   def category_id
     object.sub_category ? object.sub_category.category_id : nil
