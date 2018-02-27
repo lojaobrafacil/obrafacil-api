8fdc5368a6192f0daa2c6aa5fa4f056af7e75eaa
Arthur Moura <arthurjm95@gmail.com>
2018-02-27 18:12:04 +0000

    test

--- app/serializers/api/v1/partner_serializer.rb
+++ app/serializers/api/v1/partner_serializer.rb
@@ -2,8 +2,6 @@ class Api::V1::PartnerSerializer < ActiveModel::Serializer
   attributes :id, :name, :federal_tax_number, :state_registration, :international_registration,
   :kind, :active, :birth_date, :renewal_date, :tax_regime, :description, :order_description,
   :limit, :origin, :percent, :agency, :account, :favored, :bank, :billing_type, :user, 
-  :phones, :emails, :updated_at, :created_at
-
-  has_many :addresses, serializer: AddressSerializer
+  :phones, :emails, :addresses, :updated_at, :created_at
 
 end
