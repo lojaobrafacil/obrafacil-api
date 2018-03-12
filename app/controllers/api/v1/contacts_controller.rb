627ba115229fce42be0cf256e73d355e3aeaf538
Arthur Moura <arthurjm95@gmail.com>
2018-03-12 00:35:58 +0000

    adjusting api

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::ContactsController < Api::V1::BaseController
 
   def update_contact(model)
-    unless params_contact(model, :addresses).nil?
-      params_contact(model, :addresses).each do |address|
+    unless params_contact(:addresses).nil?
+      params_contact(:addresses).each do |address|
         ad = address.permit(:id, :street, :neighborhood, :zipcode, :ibge, :number, :complement, :description, :address_type_id, :city_id, :_destroy)
         if ad[:id] != nil
           ad[:_destroy] == true ? Address.find(ad[:id]).delete : Address.find(ad[:id]).update!(ad)
@@ -11,8 +11,8 @@ class Api::V1::ContactsController < Api::V1::BaseController
         end
       end
     end
-    unless params_contact(model, :phones).nil?
-      params_contact(model, :phones).each do |phone|
+    unless params_contact(:phones).nil?
+      params_contact(:phones).each do |phone|
         ph = phone.permit(:id, :phone, :phone_type_id, :_destroy)
         p ph
         if ph[:id] != nil
@@ -22,8 +22,8 @@ class Api::V1::ContactsController < Api::V1::BaseController
         end
       end
     end
-    unless params_contact(model, :emails).nil?
-      params_contact(model, :emails).each do |email|
+    unless params_contact(:emails).nil?
+      params_contact(:emails).each do |email|
         em = email.permit(:id, :email, :email_type_id, :_destroy)
         if em[:id] != nil
           em[:_destroy] == true ? Email.find(em[:id]).delete : Email.find(em[:id]).update!(em)
@@ -36,7 +36,7 @@ class Api::V1::ContactsController < Api::V1::BaseController
 
   private 
 
-  def params_contact(model, contact_type)
-    params.require(model.class.to_s.downcase.to_sym)[contact_type]
+  def params_contact(contact_type)
+    params.require(contact_type)
   end
 end
