80cff4de44b235c13a4c041d6c4fd3ca52160afd
Arthur Moura <arthurjm95@gmail.com>
2018-03-14 19:24:14 +0000

    adding zipcode validates numericality of

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,4 +3,6 @@ class Address < ApplicationRecord
   belongs_to :city
   belongs_to :addressable, polymorphic: true
   validates_presence_of [:street, :zipcode]
+  # validates_format_of :zipcode, :with => /[0-9]+/i, on: :save
+  validates_numericality_of :zipcode, only_integer: true
 end
