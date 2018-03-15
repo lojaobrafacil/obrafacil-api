8e7be1407b3c1a85b7b4f3b950ddaa28887050b3
Arthur Moura <arthurjm95@gmail.com>
2018-03-15 02:26:36 +0000

    testing

--- app/controllers/api/v1/partners_controller.rb
+++ app/controllers/api/v1/partners_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::PartnersController < Api::V1::ContactsController
   end
 
   def show
-    partner = policy_scope Partner
+    partner = Partner.find(params[:id])
     render json: partner, status: 200
   end
 
