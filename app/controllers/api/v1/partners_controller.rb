59f55ff6f6eb0635c5193450158214ee95067abc
Arthur Moura <arthurjm95@gmail.com>
2018-03-15 06:48:34 +0000

    adjusting show partners

--- app/controllers/api/v1/partners_controller.rb
+++ app/controllers/api/v1/partners_controller.rb
@@ -11,7 +11,7 @@ class Api::V1::PartnersController < Api::V1::ContactsController
   end
 
   def show
-    partner = partner.find(params[:id])
+    partner = Partner.find(params[:id])
     authorize partner
     render json: partner, status: 200
   end
