2cfd6371fef33013693b3fe16284b66076a9fce0
Arthur Moura <arthurjm95@gmail.com>
2018-03-19 14:22:41 +0000

    adjusting user serializer

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :federal_registration, :admin, :created_at, :updated_at
+  attributes :id, :email, :federal_registration, :kind, :created_at, :updated_at
 
   def admin
     object.admin?
