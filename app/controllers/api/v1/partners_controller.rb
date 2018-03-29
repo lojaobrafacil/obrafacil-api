cafe6a10b227a46576ecb2c5b5770ee3148af8ed
Arthur Moura <arthurjm95@gmail.com>
2018-03-29 15:27:31 +0000

    adjusting premioideal

--- app/controllers/api/v1/partners_controller.rb
+++ app/controllers/api/v1/partners_controller.rb
@@ -72,27 +72,31 @@ class Api::V1::PartnersController < Api::V1::ContactsController
     require "net/http"
     if partner.federal_tax_number? and partner.federal_tax_number.size >= 10
       body = {
-        "name": valuePremioIdeal(partner.name.as_json),
+        "name": partner.name.as_json,
         "cpfCnpj": partner.federal_tax_number.as_json,
-        "address": partner.addresses.nil? ? valuePremioIdeal(partner.addresses.first.street.as_json) : "null",
-        "number": partner.addresses.nil? ? valuePremioIdeal(partner.addresses.first.number.as_json) : "null",
-        "complement": partner.addresses.nil? ? valuePremioIdeal(partner.addresses.first.complement.as_json) : "null",
-        "cityRegion": partner.addresses.nil? ? valuePremioIdeal(partner.addresses.first.neighborhood.as_json) : "null",
-        "city": partner.addresses.nil? ? valuePremioIdeal(partner.addresses.first.city.name.as_json) : "null",
-        "state": partner.addresses.nil? ? valuePremioIdeal(partner.addresses.first.city.state.acronym.as_json) : "null",
-        "zipcode": partner.addresses.nil? ? valuePremioIdeal(partner.addresses.first.zipcode.as_json.tr("-","")) : "null",
-        "phoneDdd": partner.phones.nil? ? valuePremioIdeal(partner.phones.first.phone.delete(' ').delete("-")[0..1].as_json) : "null",
-        "phoneNumber": partner.phones.nil? ? valuePremioIdeal(partner.phones.first.phone.delete(' ').delete("-").as_json[1..9]) : "null",
-        "cellDdd": partner.phones.nil? ? valuePremioIdeal(partner.phones.first.phone.delete(' ').delete("-")[0..1].as_json) : "null",
-        "cellNumber": partner.phones.nil? ? valuePremioIdeal(partner.phones.first.phone.delete(' ').delete("-").as_json[1..9]) : "null",
-        "email": partner.emails.nil? ? valuePremioIdeal(partner.emails.first.email.as_json) : "null",
-        "birthDate": valuePremioIdeal(partner.started_date.as_json),
+        "address": partner.addresses.nil? ? "null" :  partner.addresses.first.street.as_json,
+        "number": partner.addresses.nil? ? "000" :  partner.addresses.first.number.as_json,
+        "complement": partner.addresses.nil? ? "null" :  partner.addresses.first.complement.as_json,
+        "cityRegion": partner.addresses.nil? ? "null" :  partner.addresses.first.neighborhood.as_json,
+        "city": partner.addresses.nil? ? "null" :  partner.addresses.first.city.name.as_json,
+        "state": partner.addresses.nil? ? "nd" :  partner.addresses.first.city.state.acronym.as_json,
+        "zipcode": partner.addresses.nil? ? "00000000" :  partner.addresses.first.zipcode.as_json.tr("-",""),
+        "phoneDdd": partner.phones.nil? ? "00" :  partner.phones.first.phone.delete(' ').delete("-")[0..1].as_json,
+        "phoneNumber": partner.phones.nil? ? "000000000" :  partner.phones.first.phone.delete(' ').delete("-").as_json[1..9],
+        "cellDdd": partner.phones.nil? ? "00" :  partner.phones.first.phone.delete(' ').delete("-")[0..1].as_json,
+        "cellNumber": partner.phones.nil? ? "000000000" :  partner.phones.first.phone.delete(' ').delete("-").as_json[1..9],
+        "email": partner.emails.nil? ? "null@null.com" :  partner.emails.first.email.as_json,
+        "birthDate": partner.started_date.as_json,
         "gender":0
       }
       x = Net::HTTP.post_form(URI.parse("https://homolog.markup.com.br/premioideall/webapi/api/SingleSignOn/Login?login=deca&password=deca@acesso"), body) unless Rails.env.production? # homologaçao
       x = Net::HTTP.post_form(URI.parse("https://premioideall.com.br/webapi/api/SingleSignOn/Login?login=deca&password=acesso@deca"), body) if Rails.env.production? # produçao
-      p x.body
-      p "ok parceiro: " + partner.id.to_s
+      if x.body.include?(":true")
+        p "ok parceiro: " + partner.id.to_s
+      else
+        p "Parceiro " + partner.id.to_s + " não foi para premio ideal, erro:"
+        p x.body
+      end
     else
       p "Parceiro " + partner.id.to_s + " não foi para premio ideal pois nao possue CPF/CNPJ"
     end
