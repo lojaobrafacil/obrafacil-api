3012efe360b88695668f5afb0966965ae2ba1de3
Arthur Moura <arthurjm95@gmail.com>
2018-06-06 11:02:26 -0300

    adjusting serializer

--- app/serializers/api/v1/commission_serializer.rb
+++ app/serializers/api/v1/commission_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::CommissionSerializer < ActiveModel::Serializer
-  attributes :id, :order_id, :order_date, :client_name, :order_price, :return_price
+  attributes :id, :order_id, :order_date, :client_name, :order_price, :return_price,
   :points, :percent_date, :percent, :percent_value, :sent_date, :partner_id
 
   def percent_value
