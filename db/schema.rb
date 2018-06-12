8e21c1daf9854b02a92807f2a72368e1d469b64a
Mateus Braga <mateuscbraga@gmail.com>
2018-06-12 10:24:07 -0300

    schema b_e

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606135615) do
+ActiveRecord::Schema.define(version: 20180611130411) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -419,6 +419,13 @@ ActiveRecord::Schema.define(version: 20180606135615) do
     t.index ["federal_registration"], name: "index_users_on_federal_registration", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "model"
+    t.string "brand"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "addresses", "address_types"
