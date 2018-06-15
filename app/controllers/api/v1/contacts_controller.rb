1441312f1f8165e5b54f8ee0f28194e1b1b08efa
Arthur Moura <arthurjm95@gmail.com>
2018-06-15 10:40:06 -0300

    adjusting delete contact

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -4,8 +4,8 @@ class Api::V1::ContactsController < Api::V1::BaseController
     unless params_contact(model, :addresses).nil?
       params_contact(model, :addresses).each do |address|
         ad = address.permit(:id, :street, :neighborhood, :zipcode, :ibge, :number, :complement, :description, :address_type_id, :city_id, :_destroy)
-        if ad[:id] != nil 
-          ad[:_destroy] == true ? Address.find(ad[:id]).delete : Address.find(ad[:id]).update!(ad) if Address.find(ad[:id]).addressable == model
+        if ad[:id] != nil
+          ad[:_destroy] ? Address.find(ad[:id]).delete : Address.find(ad[:id]).update!(ad) if Address.find(ad[:id]).addressable == model
         else
           begin
             model.addresses.create!(ad)
@@ -20,7 +20,7 @@ class Api::V1::ContactsController < Api::V1::BaseController
         ph = phone.permit(:id, :phone, :phone_type_id, :_destroy)
         p ph
         if ph[:id] != nil
-          ph[:_destroy] == true ? Phone.find(ph[:id]).delete : Phone.find(ph[:id]).update!(ph) if Phone.find(ph[:id]).phonable == model
+          ph[:_destroy] ? Phone.find(ph[:id]).delete : Phone.find(ph[:id]).update!(ph) if Phone.find(ph[:id]).phonable == model
         else
           begin
             model.phones.create!(ph)
@@ -34,7 +34,7 @@ class Api::V1::ContactsController < Api::V1::BaseController
       params_contact(model, :emails).each do |email|
         em = email.permit(:id, :email, :email_type_id, :_destroy)
         if em[:id] != nil
-          em[:_destroy] == true ? Email.find(em[:id]).delete : Email.find(em[:id]).update!(em) if Email.find(em[:id]).emailable == model
+          em[:_destroy] ? Email.find(em[:id]).delete : Email.find(em[:id]).update!(em) if Email.find(em[:id]).emailable == model
         else
           begin
             model.emails.create!(em)
