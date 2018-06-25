9de65cca9e6dcafbfcd357cf465269dc25c76a5b
Arthur Moura <arthurjm95@gmail.com>
2018-06-25 14:33:36 -0300

    adjusting serializer

--- app/serializers/api/v1/product_serializer.rb
+++ app/serializers/api/v1/product_serializer.rb
@@ -2,8 +2,9 @@ class Api::V1::ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :ncm, :icms, :ipi, :cest, 
       :bar_code, :reduction, :weight, :height, :width, :length,
       :kind, :active, :unit_id, :sku, :sku_xml, :sub_category_id, :provider_id,
-      :provider_name, :category_id, :images, :updated_at, :created_at
+      :provider_name, :category_id, :updated_at, :created_at
 
+  has_many :image_products
   has_many :company_products
 
   def category_id
