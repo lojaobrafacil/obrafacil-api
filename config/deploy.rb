0d712af039b7911389afe6dd729078eccf322883
Arthur Moura <arthurjm95@gmail.com>
2018-06-28 19:57:46 -0300

    adding

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :format, :airbrussh
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", ".env"
+append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
