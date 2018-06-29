21751a88898a1bf668188d0ceeda7ec041fbd2fd
Arthur Moura <arthurjm95@gmail.com>
2018-06-28 21:41:23 -0300

    adjusting provider user

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,6 @@
 class Provider < ApplicationRecord
   belongs_to :billing_type, optional: true
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :products
   has_many :phones, dependent: :destroy, as: :phonable
   has_many :addresses, dependent: :destroy, as: :addressable
