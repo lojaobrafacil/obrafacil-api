5e4915fee19268421ad4ece78808bee82e5bd256
Arthur Moura <arthurjm95@gmail.com>
2018-07-20 16:03:41 -0300

    adding

--- app/controllers/api/v1/price_percentages_controller.rb
+++ app/controllers/api/v1/price_percentages_controller.rb
@@ -4,21 +4,15 @@ class Api::V1::PricePercentagesController < Api::V1::BaseController
     price_percentages = []
 
     Company.all.each do |company|
-      pp = {"company_id": company.id, "company_name": company.name, "company_fantasy_name": company.fantasy_name}
-      count = 1
-      company.price_percentages.order(:kind).each do |price_percentage|
-        pp[("kind_"+count.to_s)]= price_percentage.kind
-        pp[("margin_"+count.to_s)]= price_percentage.margin
-        count+=1
-      end
-      price_percentages << pp
+      price_percentage = percentage_by_company(company)      
+      price_percentages << price_percentage
     end
 
     render json: price_percentages, status: 200
   end
 
   def show
-    price_percentage = PricePercentage.find(params[:id])
+    price_percentage = percentage_by_company(Company.find(params[:id]))    
     render json: price_percentage, status: 200
   end
 
@@ -31,11 +25,22 @@ class Api::V1::PricePercentagesController < Api::V1::BaseController
         nil
       end
     end
-    
-    render json: PricePercentage.where(company_id: params[:id]), status: 200
+    price_percentage = percentage_by_company(Company.find(params[:id]))
+    render json: price_percentage, status: 200
   end
 
   private
+
+  def percentage_by_company(company)
+    price_percentage = {"company_id": company.id, "company_name": company.name, "company_fantasy_name": company.fantasy_name}
+    count = 1
+    company.price_percentages.order(:kind).each do |pp|
+      price_percentage[("kind_"+count.to_s)]= pp.kind
+      price_percentage[("margin_"+count.to_s)]= pp.margin
+      count+=1
+    end
+    price_percentage
+  end
 
   def price_percentage_params
     params.require(:price_percentages)
