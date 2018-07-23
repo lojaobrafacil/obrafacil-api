c459cbb6782de6040422eaa36a994d5f75130085
Mateus Braga <mateuscbraga@gmail.com>
2018-07-23 15:51:48 -0300

    solved factories and spec - need to check a commented line on models

--- spec/factories/companies.rb
+++ spec/factories/companies.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     fantasy_name { Faker::Name.name }
     federal_tax_number { 1 }
     state_registration { 1 }
-    kind { [0,0,0,1,1,1].sample }
     birth_date { Faker::Date.birthday(18, 65) }
     tax_regime { [0,0,1,1,2,2].sample }
     description { Faker::Lorem.paragraph }
@@ -13,8 +12,7 @@ FactoryBot.define do
     pis_percent { 1 }
     confins_percent { 1 }
     icmsn_percent { 1 }
-    between_states_percent { 1 }
-    billing_type_id { create(:billing_type).id }
+    # billing_type_id { create(:billing_type).id }
     user_id { create(:user).id }
   end
 end
