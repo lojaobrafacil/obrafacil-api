83199e2ce4889276d58fe98fc6fc58726e668d40
Arthur Moura <arthurjm95@gmail.com>
2018-07-24 10:49:30 -0300

    adjusting email and phones

--- app/serializers/api/v1/email_serializer.rb
+++ app/serializers/api/v1/email_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::EmailSerializer < ActiveModel::Serializer
-  attributes :id, :email, :email_type_id, :email_type_name, :updated_at, :created_at
+  attributes :id, :email, :contact, :email_type_id, :email_type_name, :updated_at, :created_at
 
   def email_type_name
     object.email_type.name if object.email_type
--- app/serializers/api/v1/phone_serializer.rb
+++ app/serializers/api/v1/phone_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::PhoneSerializer < ActiveModel::Serializer
-  attributes :id, :phone, :phone_type_id, :phone_type_name, :updated_at, :created_at
+  attributes :id, :phone, :contact, :phone_type_id, :phone_type_name, :updated_at, :created_at
   
   def phone_type_name
     object.phone_type.name if object.phone_type
