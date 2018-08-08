9fb96a3977261ad7193d375abb3b9d4ecf07565b
Arthur Moura <arthurjm95@gmail.com>
2018-08-08 15:29:50 -0300

    adjuting for frontend

--- app/serializers/api/v2/admin/product_serializer.rb
+++ app/serializers/api/v2/admin/product_serializer.rb
@@ -1,8 +1,9 @@
 class Api::V2::Admin::ProductSerializer < ActiveModel::Serializer
   attributes :id, :code, :name, :description, :ncm, :icms, :ipi, :cest, 
       :bar_code, :reduction, :weight, :height, :width, :length,
-      :kind, :active, :unit_id, :sku, :sku_xml, :sub_category_id, :supplier_id,
-      :supplier_name, :supplier_fantasy_name, :category_id, :updated_at, :created_at, :images
+      :kind, :active, :unit_id, :unit_name, :sku, :sku_xml, :sub_category_id, 
+      :sub_category_name, :supplier_id, :supplier_name, :supplier_fantasy_name, 
+      :category_id, :category_name, :updated_at, :created_at, :images
       
   has_many :company_products
 
@@ -12,6 +13,18 @@ class Api::V2::Admin::ProductSerializer < ActiveModel::Serializer
 
   def category_id
     object.sub_category ? object.sub_category.category_id : nil
+  end
+
+  def category_name
+    object.sub_category ? object.sub_category.category.name : nil
+  end
+
+  def sub_category_name
+    object.sub_category ? object.sub_category.name : nil
+  end
+
+  def unit_name
+    object.unit ? object.unit.name : nil
   end
 
   def supplier_name
