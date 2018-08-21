381e28639d505500a11caeff867f569aff1f915c
Arthur Moura <arthurjm95@gmail.com>
2018-08-21 11:13:39 -0300

    product policy

---
+++ app/policies/admin/product_policy.rb
@@ -0,0 +1,28 @@
+class Admin::ProductPolicy < Admin::ApplicationPolicyV2
+  
+  def show?
+    Product.where(:id => record.id).exists? && (user.change_products || user.admin)
+  end
+
+  def destroy?
+    user.admin
+  end
+
+  def permitted_attributes
+    if user.change_products || user.admin
+      [:code, :name, :description, :ncm, :icms, :ipi, :cest, 
+        :bar_code, :reduction, :weight, :height, :width, :length, :supplier_id,
+        :kind, :active, :unit_id, :sku, :sku_xml, :sub_category_id]
+    else
+      []
+    end
+  end
+
+  class Scope < Scope
+    def resolve
+      if user.change_products || user.admin
+        scope.all
+      end
+    end
+  end
+end
