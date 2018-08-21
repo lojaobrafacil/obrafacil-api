c34146ec6eff48ea16c14d24ef61d3992405cc10
Arthur Moura <arthurjm95@gmail.com>
2018-08-21 11:43:15 -0300

    adjusting image policy

--- app/policies/admin/image_policy.rb
+++ app/policies/admin/image_policy.rb
@@ -3,11 +3,7 @@ class Admin::ProductPolicy < Admin::ApplicationPolicyV2
   def create?
     Product.where(:id => record.id).exists? && (user.change_products || user.admin)
   end
-
-  def destroy?
-    user.admin
-  end
-
+  
   def permitted_attributes
     if user.change_products || user.admin
       [:images]
