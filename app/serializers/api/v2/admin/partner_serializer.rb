cceb689ad2c3175c4ca75f4c17b834ac19658d44
Arthur Moura <arthurjm95@gmail.com>
2018-08-21 14:11:16 -0300

    changing organization

--- app/serializers/api/v2/admin/partner_serializer.rb
+++ app/serializers/api/v2/admin/partner_serializer.rb
@@ -1,8 +1,8 @@
 class Api::V2::Admin::PartnerSerializer < ActiveModel::Serializer
   attributes :id, :name, :federal_registration, :state_registration, :kind, :active, 
   :started_date, :renewal_date, :description, :origin, :percent, :agency, :account,
-  :favored, :bank_id, :bank_name, :ocupation, :user, :discount3, :discount5, :discount8, :cash_redemption,
-  :updated_at, :created_at
+  :favored, :bank_id, :bank_name, :ocupation, :user, :discount3, :discount5, :discount8, 
+  :cash_redemption, :updated_at, :created_at
 
   has_many :addresses
   has_many :phones
