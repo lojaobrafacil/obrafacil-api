5447abaec82c3b1361ad702fb1033460b8f7338a
Arthur Moura <arthurjm95@gmail.com>
2018-08-22 14:00:00 -0300

    adding change_suppliers

--- app/serializers/api/v2/admin/employee_serializer.rb
+++ app/serializers/api/v2/admin/employee_serializer.rb
@@ -1,8 +1,8 @@
 class Api::V2::Admin::EmployeeSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :federal_registration, :state_registration, :active,
-  :birth_date, :renewal_date, :admin, :change_partners, :change_clients, :change_cashiers, 
-  :generate_nfe, :import_xml, :change_products, :order_client, :order_devolution, :order_cost, 
-  :order_done, :order_price_reduce, :order_inactive, :order_creation, :limit_price_percentage, 
+  :birth_date, :renewal_date, :admin, :change_partners, :change_clients, :change_suppliers, 
+  :change_cashiers, :generate_nfe, :import_xml, :change_products, :order_client, :order_devolution,
+  :order_cost, :order_done, :order_price_reduce, :order_inactive, :order_creation, :limit_price_percentage, 
   :commission_percent, :description, :updated_at, :created_at
 
   has_many :addresses
