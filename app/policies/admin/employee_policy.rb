3232561864edcc77475045295e9ba441338e9894
Arthur Moura <arthurjm95@gmail.com>
2018-08-27 16:09:33 -0300

    adding change_suppliers to employees

--- app/policies/admin/employee_policy.rb
+++ app/policies/admin/employee_policy.rb
@@ -11,7 +11,7 @@ class Admin::EmployeePolicy < Admin::ApplicationPolicyV2
   def permitted_attributes
     if user.admin
       [:name, :email, :federal_registration, :state_registration, :active,
-        :birth_date, :renewal_date, :admin, :change_partners, :change_clients, :change_cashiers, 
+        :birth_date, :renewal_date, :admin, :change_partners, :change_suppliers, :change_clients, :change_cashiers, 
         :generate_nfe, :import_xml, :change_products, :order_client, :order_devolution, :order_cost, 
         :order_done, :order_price_reduce, :order_inactive, :order_creation, :limit_price_percentage, 
         :commission_percent, :description]
