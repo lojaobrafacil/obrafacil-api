146b373f6a856a32a71cc0b66b15f38b15ef88d0
Arthur Moura <arthurjm95@gmail.com>
2018-09-20 21:40:40 -0300

    adjusting log

--- app/serializers/api/v2/admin/log/premio_ideal_serializer.rb
+++ app/serializers/api/v2/admin/log/premio_ideal_serializer.rb
@@ -1,4 +1,7 @@
 class Api::V2::Admin::Log::PremioIdealSerializer < ActiveModel::Serializer
-  attributes :id, :status, :error, :body
-  has_one :partner
+  attributes :id, :status, :error, :body, :partner_federal_registration
+
+  def partner_federal_registration
+    object.partner ? object.partner.federal_registration : nil
+  end
 end
