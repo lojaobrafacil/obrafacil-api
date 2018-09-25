4ee0ec43e0989cc62089a41acfd101d42938e512
Arthur Moura <arthurjm95@gmail.com>
2018-09-25 11:33:06 -0300

    resolvendo o problema com parceiro inativo

--- app/controllers/api/v2/partner/partners_controller.rb
+++ app/controllers/api/v2/partner/partners_controller.rb
@@ -1,87 +1,8 @@
 class Api::V2::PartnersController < Api::V2::Partner::ContactsController
   def show
-    partner = current_api_v1_user ?  Partner.find_by(user_id: current_api_v1_user.id) : Partner.find(params[:id])
+    partner = current_api_v1_user ?  Partner.find_by(user_id: current_api_v1_user.id) : render json: {errors: "Não Foi possivel encontrar o usuario"}
     partner = Partner.find(params[:id]) if current_api_v1_user&.admin?
     # authorize partner
     render json: partner, status: 200
   end
-
-  def update
-    partner = Partner.find(params[:id])
-    # authorize partner
-    if partner.update(partner_params)
-      update_contact(partner)
-      update_user(partner)
-      premio_ideal(partner)      
-      render json: partner, status: 200
-    else
-      render json: { errors: partner.errors }, status: 422
-    end
-  end
-  
-  private
-
-  def partner_params
-      params.permit(:id, :name, :federal_registration, :state_registration, 
-      :kind, :active, :started_date, :renewal_date, :description, :origin, :percent, :agency, 
-      :ocupation, :account, :favored, :user_id, :bank_id, :discount3, :discount5, :discount8, :cash_redemption)
-  end
-
-  def update_user(partner)
-    if user = User.find_by(federal_registration: partner.federal_registration)
-      if partner.active?
-        user.update(partner: partner) unless user.partner == partner 
-      else
-        user.destroy unless user.partner.active?
-      end
-    else
-      email = partner.federal_registration? ? partner.federal_registration.to_s+"@obrafacil.com" : partner.emails.first.email rescue nil
-      unless email&.nil?
-        partner.build_user(email: email,
-                            federal_registration: partner.federal_registration,
-                            password:"obrafacil2018",
-                            password_confirmation:"obrafacil2018" ).save
-      end
-    end
-  end
-
-  def premio_ideal(partner)
-    require "uri"
-    require "net/http"
-    if partner.federal_registration? and partner.federal_registration.size >= 10 and partner.active?
-      begin
-        body = {
-          "name": partner.name.as_json,
-          "cpfCnpj": partner.federal_registration.as_json,
-          "address": if partner.addresses.nil? ; "null"; elsif  partner.addresses.first.street.nil? ||  partner.addresses.first.street == ""; "null"; else partner.addresses.first.street.as_json end,
-          "number": if partner.addresses.nil? ; "000"; elsif  partner.addresses.first.number.nil? ||  partner.addresses.first.number == ""; "000"; else partner.addresses.first.number.as_json end,
-          "complement": if partner.addresses.nil? ; "null"; elsif  partner.addresses.first.complement.nil? ||  partner.addresses.first.complement == ""; "null"; else partner.addresses.first.complement.as_json end,
-          "cityRegion": if partner.addresses.nil? ; "null"; elsif  partner.addresses.first.neighborhood.nil? ||  partner.addresses.first.neighborhood == ""; "null"; else partner.addresses.first.neighborhood.as_json end,
-          "city": if partner.addresses.nil? ; "null"; elsif  partner.addresses.first.city.nil? ||  partner.addresses.first.city == ""; "null"; else partner.addresses.first.city.name.as_json end,
-          "state": if partner.addresses.nil? ; "nd"; elsif  partner.addresses.first.city.nil? ||  partner.addresses.first.city == ""; "nd"; else partner.addresses.first.city.state.acronym.as_json end,
-          "zipcode": if partner.addresses.nil? ; "00000000"; elsif  partner.addresses.first.zipcode.nil? ||  partner.addresses.first.zipcode == ""; "00000000"; else partner.addresses.first.zipcode.as_json.tr("-","") end,
-          "phoneDdd": partner.phones.nil? ? "00" :  partner.phones.first.phone.delete(' ').delete("-")[0..1].as_json,
-          "phoneNumber": partner.phones.nil? ? "000000000" :  partner.phones.first.phone.delete(' ').delete("-").as_json[1..9],
-          "cellDdd": partner.phones.nil? ? "00" :  partner.phones.first.phone.delete(' ').delete("-")[0..1].as_json,
-          "cellNumber": partner.phones.nil? ? "000000000" :  partner.phones.first.phone.delete(' ').delete("-").as_json[1..9],
-          "email": partner.emails.nil? ? "null@null.com" :  partner.emails.first.email.as_json,
-          "birthDate": partner.started_date.as_json,
-          "gender":0
-        }
-        x = Net::HTTP.post_form(URI.parse("https://homolog.markup.com.br/premioideall/webapi/api/SingleSignOn/Login?login=deca&password=deca@acesso"), body) unless Rails.env.production? # homologaçao
-        x = Net::HTTP.post_form(URI.parse("https://premioideall.com.br/webapi/api/SingleSignOn/Login?login=deca&password=deca@acesso"), body) if Rails.env.production? # produçao
-        if x.body.include?(":true")
-          p "ok parceiro: " + partner.name
-        else
-          p "Parceiro " + partner.name + " não foi para premio ideal, erro:"
-          p x.body
-        end
-      rescue
-        "erro ao processar " + partner.name + " favor confirmar se o cadastro esta correto"
-      end
-    else
-      p "Parceiro " + partner.name + " não foi para premio ideal pois nao possue CPF/CNPJ ou esta inativo"
-    end
-  end
-  
 end
--- app/models/partner.rb
+++ app/models/partner.rb
@@ -16,7 +16,7 @@ class Partner < ApplicationRecord
   validates_presence_of :name, :kind
   validates_uniqueness_of :federal_registration, scope: :active 
   include Contact
-  after_save :update_user
+  after_save :update_user, :premio_ideal
   
   def self.active; where("active = true").order(:id); end
   def self.inactive; where("active = false").order(:id); end
@@ -34,8 +34,54 @@ class Partner < ApplicationRecord
           password_confirmation:"obrafacil2018" ).save
       end
     else
-      user = self.user
-      user.destroy if !user.client&.active?
+      self.user.update(password: 'desativado', password_confirmation: 'desativado') unless self.user&.client&.active?
     end
+  end
+
+  def premio_ideal
+    partner_id = self.id
+    partner = Partner.find(partner_id)
+    body = ""
+    if partner.federal_registration? and partner.federal_registration.size >= 10 and partner.active?
+      begin
+        body = body_params(partner)
+        x = Net::HTTP.post_form(URI.parse(premio_ideal_url), body)
+        status = x.code ? x.code.to_i : 422
+        if status == 200
+          Log::PremioIdeal.create(partner_id: partner_id, body: body.to_s, status: status, error: x.msg)
+        else
+          Log::PremioIdeal.create(partner_id: partner_id, body: body.to_s, status: status, error: ("Parceiro " + partner.name + " não foi para premio ideal, erro:"))
+        end
+      rescue
+        Log::PremioIdeal.create(partner_id: partner_id, body: body.to_s, error: ("erro ao processar " + partner.name + " favor confirmar se o cadastro esta correto").as_json, status: 422)
+      end
+    else
+      Log::PremioIdeal.create!(partner_id: partner_id, error: ("Parceiro " + partner.name + " não foi para premio ideal pois nao possue CPF/CNPJ ou esta inativo").as_json, status: status, body: nil)
+    end
+  end
+
+  def premio_ideal_url
+    Rails.env.production? ? "https://premioideall.com.br/webapi/api/SingleSignOn/Login?login=deca&password=deca@acesso" : "https://homolog.markup.com.br/premioideall/webapi/api/SingleSignOn/Login?login=deca&password=deca@acesso"
+  end
+
+  def body_params(partner)
+    {
+      "name": partner.name.as_json,
+      "cpfCnpj": partner.federal_registration.as_json,
+      "address": if partner.addresses.empty? ; "null"; elsif  partner.addresses.first.street.nil? ||  partner.addresses.first.street == ""; "null"; else partner.addresses.first.street.as_json end,
+      "number": if partner.addresses.empty? ; "000"; elsif  partner.addresses.first.number.nil? ||  partner.addresses.first.number == ""; "000"; else partner.addresses.first.number.as_json end,
+      "complement": if partner.addresses.empty? ; "null"; elsif  partner.addresses.first.complement.nil? ||  partner.addresses.first.complement == ""; "null"; else partner.addresses.first.complement.as_json end,
+      "cityRegion": if partner.addresses.empty? ; "null"; elsif  partner.addresses.first.neighborhood.nil? ||  partner.addresses.first.neighborhood == ""; "null"; else partner.addresses.first.neighborhood.as_json end,
+      "city": if partner.addresses.empty? ; "null"; elsif  partner.addresses.first.city.nil? ||  partner.addresses.first.city == ""; "null"; else partner.addresses.first.city.name.as_json end,
+      "state": if partner.addresses.empty? ; "nd"; elsif  partner.addresses.first.city.nil? ||  partner.addresses.first.city == ""; "nd"; else partner.addresses.first.city.state.acronym.as_json end,
+      "zipcode": if partner.addresses.empty? ; "00000000"; elsif  partner.addresses.first.zipcode.nil? ||  partner.addresses.first.zipcode == ""; "00000000"; else partner.addresses.first.zipcode.as_json.tr("-","") end,
+      "phoneDdd": partner.phones.empty? ? "00" :  partner.phones.first.phone.delete(' ').delete("-")[0..1].as_json,
+      "phoneNumber": partner.phones.empty? ? "000000000" :  partner.phones.first.phone.delete(' ').delete("-").as_json[1..9],
+      "cellDdd": partner.phones.empty? ? "00" :  partner.phones.first.phone.delete(' ').delete("-")[0..1].as_json,
+      "cellNumber": partner.phones.empty? ? "000000000" :  partner.phones.first.phone.delete(' ').delete("-").as_json[1..9],
+      "email": partner.emails.empty? ? "null@null.com" :  partner.emails.first.email.as_json,
+      "birthDate": partner.started_date.as_json,
+      "gender":0
+    }
   end
 end
