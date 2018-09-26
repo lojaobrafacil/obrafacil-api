b16d86e7c5d145ae9f3fe300c4b0b15113b0c025
Arthur Moura <arthurjm95@gmail.com>
2018-09-26 04:35:13 -0300

    adjusting

--- app/controllers/api/v2/admin/users_controller.rb
+++ app/controllers/api/v2/admin/users_controller.rb
@@ -2,7 +2,8 @@ class Api::V2::Admin::UsersController < Api::V2::Admin::BaseController
 
   def reset_password
     authorize [:admin, User]
-    if User.find_by(federal_registration: params[:federal_registration])&.reset_password(params[:password], params[:password_confirmation])
+    user = User.where(federal_registration: params[:federal_registration]).first
+    if user&.reset_password(params[:password], params[:password_confirmation])
       render json: { status: "Ok" }, status: 201
     else 
       render json: { errors: {error: "usuario não existe ou password_confirmation incorreto"} }, status: 422
