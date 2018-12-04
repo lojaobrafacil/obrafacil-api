20588160b7b7ec6435e7b675d57b8c5993e079a7
Arthur Moura <arthurjm95@gmail.com>
2018-12-04 20:06:14 -0200

    adding method auth api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
+  def authenticate_admin_or_api!
+    if headers[:access_id] && headers[:access_key]
+      current_user = Api.find_by(access_id: headers[:access_id], access_key: headers[:access_key])
+      if current_user&.active
+        return true
+      elsif !current_user&.active
+        return render json: { error: I18n.t('devise.failure.inactive') }, status: 422
+      else
+        return render json: { error: I18n.t('devise.failure.unauthenticated') }, status: 422
+      end
+    else
+      authenticate_api_v2_admin_employee!
+    end
+  end
 end
