5a31e2495aed79f5948d0ddc904f6bc38f9dfd98
Arthur Moura <arthurjm95@gmail.com>
2018-12-05 02:48:40 -0200

    add migration content

--- db/migrate/20181205043645_create_apis.rb
+++ db/migrate/20181205043645_create_apis.rb
@@ -1,13 +1,13 @@
 class CreateApis < ActiveRecord::Migration[5.2]
   def change
-    # create_table :apis do |t|
-    #   t.string :name
-    #   t.string :federal_registration
-    #   t.string :access_id
-    #   t.string :access_key
-    #   t.boolean :active, default: true
+    create_table :apis do |t|
+      t.string :name
+      t.string :federal_registration
+      t.string :access_id
+      t.string :access_key
+      t.boolean :active, default: true
 
-    #   t.timestamps
-    # end
+      t.timestamps
+    end
   end
 end
