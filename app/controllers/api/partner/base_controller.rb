abb895db0359aae44007c6a28221a6a2980b277a
Arthur Moura <arthurjm95@gmail.com>
2018-12-05 11:51:29 -0200

    adjusting

--- app/controllers/api/partner/base_controller.rb
+++ app/controllers/api/partner/base_controller.rb
@@ -1,8 +1,8 @@
 class Api::Partner::BaseController < ApplicationController
-  before_action :authenticate_api_v1_user!
+  before_action :authenticate_api_client_user!
   include Pundit
   def pundit_user 
-    current_api_v2_user
+    current_api_client_user
   end
   
 end
--- app/controllers/api/partner/partners_controller.rb
+++ app/controllers/api/partner/partners_controller.rb
@@ -1,6 +1,6 @@
 class Api::Partner::PartnersController < Api::Partner::BaseController
   def index
-    partner = current_api_v1_user.partner
+    partner = current_api_client_user.partner
     render json: partner, status: 200
   end
 end
