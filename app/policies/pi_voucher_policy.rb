4f2c6ad020e24dc5c4ae1c43af42c8ff1bdbed0e
Arthur Moura <arthurjm95@gmail.com>
2019-03-19 16:06:57 -0300

    remove policy

--- app/policies/pi_voucher_policy.rb
+++ app/policies/pi_voucher_policy.rb
@@ -1,17 +1,18 @@
 class PiVoucherPolicy < ApplicationPolicy
   def show?
-    PiVoucher.where(:id => record.id).exists? && (user.is_a?(Api) || user&.change_partners || user&.admin)
+    true
+    # PiVoucher.where(:id => record.id).exists? && (user.is_a?(Api) || user&.change_partners || user&.admin)
   end
 
   def permitted_attributes
-    if user.is_a?(Api) || user&.change_partners || user&.admin
+    if true
       [:value, :used_at, :status, :received_at, :company_id, :partner_id]
     end
   end
 
   class Scope < Scope
     def resolve
-      if user.is_a?(Api) || user&.change_partners || user&.admin
+      if true
         scope.all
       end
     end
