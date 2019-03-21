1530c0f1cc154fc454e67abe8ba06bd127ac84d4
Arthur Moura <arthurjm95@gmail.com>
2019-03-21 11:19:37 -0300

    adjusting reports

--- app/workers/report_upload_worker.rb
+++ app/workers/report_upload_worker.rb
@@ -6,10 +6,9 @@ class ReportUploadWorker
     case obj["type"]
     when "MODEL"
       model = obj["model"].classify.constantize.all
-      reports = Report.where("created_at BETWEEN ? AND ?", Time.now - 1.min, Time.now + 1.min)
-      if reports.empty?
+      if Report.where("name: ? and created_at BETWEEN ? AND ?", obj["model"].upcase, Time.now - 1.min, Time.now + 1.min).empty?
         path = ToXlsx.new(model, { titles: obj["titles"], attributes: obj["fields"], filename: obj["pathname"] }).generate
-        rep = Report.create(name: "PARTNER", employee_id: obj["user_id"], attachment: File.new(path))
+        rep = Report.create(name: obj["model"].upcase, employee_id: obj["user_id"], attachment: File.new(path))
         File.delete(path) if File.exist?(path)
         Pusher.trigger("reports-#{obj["user_id"]}", "partner", { message: "Seu relatório está pronto.", file: rep.attachment })
       end
