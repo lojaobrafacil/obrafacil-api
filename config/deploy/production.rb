6413aed7a8de5750d5d50fe7d942645f5e526264
Arthur Moura <arthurjm95@gmail.com>
2019-03-21 00:45:11 -0300

    adjustin

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server "165.227.180.52", user: "deploy", roles: %w{app db web}
+server "165.227.180.52", user: "deploy", roles: %w{app web}
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 set :sidekiq_role, :app
