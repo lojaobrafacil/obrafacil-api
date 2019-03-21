aa0061c47e7510208e15a5e8f33b1d1fda5d2fa4
Arthur Moura <arthurjm95@gmail.com>
2019-03-21 11:38:50 -0300

    adjusting methods

--- app/workers/report_upload_worker.rb
+++ app/workers/report_upload_worker.rb
@@ -6,7 +6,7 @@ class ReportUploadWorker
     case obj["type"]
     when "MODEL"
       model = obj["model"].classify.constantize.all
-      if Report.where("name: ? and created_at BETWEEN ? AND ?", obj["model"].upcase, Time.now - 1.minute, Time.now + 1.minute).empty?
+      if Report.where("name = ? and created_at BETWEEN ? AND ?", obj["model"].upcase, Time.now - 1.minute, Time.now + 1.minute).count == 0
         path = ToXlsx.new(model, { titles: obj["titles"], attributes: obj["fields"], filename: obj["pathname"] }).generate
         rep = Report.create(name: obj["model"].upcase, employee_id: obj["user_id"], attachment: File.new(path))
         File.delete(path) if File.exist?(path)
