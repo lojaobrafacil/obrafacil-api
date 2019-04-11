286548948e65a351dd790b56cc37e79891d83f6b
Arthur Moura <arthurjm95@gmail.com>
2019-04-11 11:04:37 -0300

    adjusting sms message

--- app/workers/sms_partners_worker.rb
+++ app/workers/sms_partners_worker.rb
@@ -3,6 +3,6 @@ class SmsPartnersWorker
 
   def perform(obj)
     @partners = ::Partner.where(id: obj["partner_ids"])
-    @partners.map { |partner| Notifications::SmsService.new(partner.phone, "[ObraFacil] Solicitamos que faça o resgate dos seus pontos no programa de fidelidade até o final deste mês. Dúvidas ligar 11 3031-6891").call }
+    @partners.map { |partner| Notifications::SmsService.new(partner.phone, "[ObraFacil] Solicitamos que faça o resgate dos seus pontos em dinheiro no programa de fidelidade até o final deste mês, Dúvidas 11 3031-6891").call }
   end
 end
