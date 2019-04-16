9d8c9a52fc047aad89afe1a1e78ee98ab39faf98
Arthur Moura <arthurjm95@gmail.com>
2019-04-16 11:22:48 -0300

    test

--- app/services/pi_vouchers/pdf_service.rb
+++ app/services/pi_vouchers/pdf_service.rb
@@ -38,46 +38,46 @@ module PiVouchers
     end
 
     def render
-      begin
-        pdf = Prawn::Document.new(PDF_OPTIONS) do |pdf|
-          pdf.image open("https://images-americanas.b2w.io/marketplace/logo/grande/5909339000129.jpg"), width: 100, :at => [380, 90]
-          pdf.move_down 30
+      # begin
+      pdf = Prawn::Document.new(PDF_OPTIONS) do |pdf|
+        pdf.image open("https://images-americanas.b2w.io/marketplace/logo/grande/5909339000129.jpg"), width: 100, :at => [380, 90]
+        pdf.move_down 30
 
-          pdf.fill_color "225599"
-          pdf.text "Obra Fácil", size: 40, style: :bold, align: :left
+        pdf.fill_color "225599"
+        pdf.text "Obra Fácil", size: 40, style: :bold, align: :left
 
-          pdf.move_down 30
-          pdf.fill_color "40464e"
-          pdf.text "Voucher - Nro. #{@voucher.id}", size: 20, style: :bold, align: :left
+        pdf.move_down 30
+        pdf.fill_color "40464e"
+        pdf.text "Voucher - Nro. #{@voucher.id}", size: 20, style: :bold, align: :left
 
-          pdf.move_down 30
-          pdf.text "Esse voucher foi emitido para <b>#{@voucher.partner.name.strip}</b>", inline_format: true
+        pdf.move_down 30
+        pdf.text "Esse voucher foi emitido para <b>#{@voucher.partner.name.strip}</b>", inline_format: true
 
-          pdf.move_down 15
-          pdf.text "Valor: #{@voucher.value_br}."
+        pdf.move_down 15
+        pdf.text "Valor: #{@voucher.value_br}."
 
-          pdf.move_down 15
-          pdf.text "Data de Emissão #{@voucher.created_at.strftime("%d/%m/%Y")}."
+        pdf.move_down 15
+        pdf.text "Data de Emissão #{@voucher.created_at.strftime("%d/%m/%Y")}."
 
-          pdf.move_down 15
-          pdf.text "Expira em #{@voucher.expiration_date.strftime("%d/%m/%Y")}."
+        pdf.move_down 15
+        pdf.text "Expira em #{@voucher.expiration_date.strftime("%d/%m/%Y")}."
 
-          pdf.draw_text "O valor do voucher deverá ser usado integralmente na compra,", :at => [0, 300], :size => 10
-          pdf.draw_text "Sendo assim, a compra deverá ter um valor total igual ou superior ao voucher.", :at => [0, 290], :size => 10
-          pdf.draw_text "Valido somente para utilização em lojas físicas.", :at => [0, 250], :size => 10
+        pdf.draw_text "O valor do voucher deverá ser usado integralmente na compra,", :at => [0, 300], :size => 10
+        pdf.draw_text "Sendo assim, a compra deverá ter um valor total igual ou superior ao voucher.", :at => [0, 290], :size => 10
+        pdf.draw_text "Valido somente para utilização em lojas físicas.", :at => [0, 250], :size => 10
 
-          pdf.draw_text "https://www.lojaobrafacil.com.br", :at => [0, 0], :size => 10
-          pdf.render_file(@path)
-        end
-
-        if file = File.new(@path)
-          @voucher.update(attachment: file)
-          File.delete(@path)
-        end
-      rescue
-        File.delete(@path) rescue nil
-        return add_error({ error: "Erro ao gerar PDF, tente novamente", content: @voucher }, 404)
+        pdf.draw_text "https://www.lojaobrafacil.com.br", :at => [0, 0], :size => 10
+        pdf.render_file(@path)
       end
+
+      if file = File.new(@path)
+        @voucher.update(attachment: file)
+        File.delete(@path)
+      end
+      # rescue
+      #   File.delete(@path)
+      #   return add_error({ error: "Erro ao gerar PDF, tente novamente", content: @voucher }, 404)
+      # end
       return { success: true, message: "Processado com sucesso", path: @path, status: 200 }
     end
   end
