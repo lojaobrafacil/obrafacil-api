cd64ad9d799aee43876fac21984df3b0dccafe97
Arthur Moura <arthurjm95@gmail.com>
2019-04-15 23:10:02 -0300

    adjusting

--- app/workers/sms_partners_worker.rb
+++ app/workers/sms_partners_worker.rb
@@ -6,11 +6,15 @@ class SmsPartnersWorker
     @log_messages = { success: [], errors: [] }
     @partners = ::Partner.where(id: obj["partner_ids"])
     @partners.map { |partner|
-      service = Notifications::SmsService.new(partner.phone.phone, "[ObraFacil]Ola #{partner.phone.contact}!Troque seus pontos acumulados em dinheiro ou em produtos da nossa loja até o fim deste mês! bit.ly/2D8RyJ7")
-      if service.call
-        @log_messages[:success] << { partner_id: partner.id, phone_number: partner.phone.phone, message: service.message }
-      else
-        @log_messages[:errors] << { partner_id: partner.id, phone_number: partner.phone.phone, message: service.error_message }
+      @name = partner.phone.contact.split(" ")[0] rescue nil
+      @phone = partner.phone.phone rescue nil
+      if @phone
+        service = Notifications::SmsService.new(@phone, "[ObraFacil]Ola #{@name}!Troque seus pontos acumulados em dinheiro ou em produtos da nossa loja até o fim deste mês! bit.ly/2D8RyJ7")
+        if service.call
+          @log_messages[:success] << { partner_id: partner.id, phone_number: @phone, message: service.message }
+        else
+          @log_messages[:errors] << { partner_id: partner.id, phone_number: @phone, message: service.error_message }
+        end
       end
     }
     @log.update(finished_at: Time.now, content: @log_messages, status: "OK")
