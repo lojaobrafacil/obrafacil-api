7f204e7bb0311325e19c01719f0a2db77d63cd2c
Arthur Moura <arthurjm95@gmail.com>
2019-04-16 12:54:21 -0300

    adjusting

--- app/services/pi_vouchers/pdf_service.rb
+++ app/services/pi_vouchers/pdf_service.rb
@@ -10,7 +10,7 @@ module PiVouchers
     def initialize(voucher)
       Prawn::Font::AFM.hide_m17n_warning = true
       @voucher = voucher
-      @path = Rails.root.join("public/pdf/voucher_#{@voucher.id}.pdf")
+      @path = "voucher_#{@voucher.id}.pdf"
       super()
     end
 
@@ -39,46 +39,45 @@ module PiVouchers
 
     def render
       begin
-        pdf = Prawn::Document.new(PDF_OPTIONS) do |pdf|
-          pdf.image open("https://images-americanas.b2w.io/marketplace/logo/grande/5909339000129.jpg"), width: 100, :at => [380, 90]
-          pdf.move_down 30
+        pdf = Prawn::Document.new(PDF_OPTIONS) do |pdf_op|
+          pdf_op.image open("https://images-americanas.b2w.io/marketplace/logo/grande/5909339000129.jpg"), width: 100, :at => [380, 90]
+          pdf_op.move_down 30
 
-          pdf.fill_color "225599"
-          pdf.text "Obra Fácil", size: 40, style: :bold, align: :left
+          pdf_op.fill_color "225599"
+          pdf_op.text "Obra Fácil", size: 40, style: :bold, align: :left
 
-          pdf.move_down 30
-          pdf.fill_color "40464e"
-          pdf.text "Voucher - Nro. #{@voucher.id}", size: 20, style: :bold, align: :left
+          pdf_op.move_down 30
+          pdf_op.fill_color "40464e"
+          pdf_op.text "Voucher - Nro. #{@voucher.id}", size: 20, style: :bold, align: :left
 
-          pdf.move_down 30
-          pdf.text "Esse voucher foi emitido para <b>#{@voucher.partner.name.strip}</b>", inline_format: true
+          pdf_op.move_down 30
+          pdf_op.text "Esse voucher foi emitido para <b>#{@voucher.partner.name.strip}</b>", inline_format: true
 
-          pdf.move_down 15
-          pdf.text "Valor: #{@voucher.value_br}."
+          pdf_op.move_down 15
+          pdf_op.text "Valor: #{@voucher.value_br}."
 
-          pdf.move_down 15
-          pdf.text "Data de Emissão #{@voucher.created_at.strftime("%d/%m/%Y")}."
+          pdf_op.move_down 15
+          pdf_op.text "Data de Emissão #{@voucher.created_at.strftime("%d/%m/%Y")}."
 
-          pdf.move_down 15
-          pdf.text "Expira em #{@voucher.expiration_date.strftime("%d/%m/%Y")}."
+          pdf_op.move_down 15
+          pdf_op.text "Expira em #{@voucher.expiration_date.strftime("%d/%m/%Y")}."
 
-          pdf.draw_text "O valor do voucher deverá ser usado integralmente na compra,", :at => [0, 300], :size => 10
-          pdf.draw_text "Sendo assim, a compra deverá ter um valor total igual ou superior ao voucher.", :at => [0, 290], :size => 10
-          pdf.draw_text "Valido somente para utilização em lojas físicas.", :at => [0, 250], :size => 10
+          pdf_op.draw_text "O valor do voucher deverá ser usado integralmente na compra,", :at => [0, 300], :size => 10
+          pdf_op.draw_text "Sendo assim, a compra deverá ter um valor total igual ou superior ao voucher.", :at => [0, 290], :size => 10
+          pdf_op.draw_text "Valido somente para utilização em lojas físicas.", :at => [0, 250], :size => 10
 
-          pdf.draw_text "https://www.lojaobrafacil.com.br", :at => [0, 0], :size => 10
-          pdf.render_file(@path)
+          pdf_op.draw_text "https://www.lojaobrafacil.com.br", :at => [0, 0], :size => 10
         end
-
-        if file = File.new(@path)
-          @voucher.update(attachment: file)
-          File.delete(@path)
-        end
+        tmpfile = Tempfile.new(@path)
+        tmpfile.binmode
+        tmpfile.write pdf.render
+        @voucher.update(attachment: tmpfile)
+        tmpfile.close
+        tmpfile.unlink
       rescue
-        File.delete(@path) rescue nil
         return add_error({ error: "Erro ao gerar PDF, tente novamente", content: @voucher }, 404)
       end
-      return { success: true, message: "Processado com sucesso", path: @path, status: 200 }
+      return { success: true, message: "Processado com sucesso", status: 200 }
     end
   end
 end
--- app/uploaders/report_file_uploader.rb
+++ app/uploaders/report_file_uploader.rb
@@ -4,4 +4,8 @@ class ReportFileUploader < CarrierWave::Uploader::Base
   def store_dir
     "#{model.class.to_s.underscore}"
   end
+
+  def filename
+    "voucher_#{model.id}.pdf"
+  end
 end
