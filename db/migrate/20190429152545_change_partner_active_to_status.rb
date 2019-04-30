b050294b8e6f11f8a1d139d507b36f2f44649aeb
Arthur Moura <arthurjm95@gmail.com>
2019-04-30 08:48:30 -0300

    adjusting migration

--- db/migrate/20190429152545_change_partner_active_to_status.rb
+++ db/migrate/20190429152545_change_partner_active_to_status.rb
@@ -1,17 +1,15 @@
 class ChangePartnerActiveToStatus < ActiveRecord::Migration[5.2]
   def self.up
     add_column :partners, :status, :integer
-    Partner.all.each do |partner|
-      partner.active ? partner.update(status: 1) : partner.update(status: 2)
-    end
+    Partner.where(active: true).update_all(status: 1)
+    Partner.where.not(active: true).update_all(status: 2)
     remove_column :partners, :active
   end
 
   def self.down
     add_column :partners, :active, :boolean
-    Partner.all.each do |partner|
-      partner.status == 1 ? partner.update(active: true) : partner.update(active: false)
-    end
+    Partner.where(status: 1).update_all(active: true)
+    Partner.where.not(status: 1).update_all(active: false)
     remove_column :partners, :status
   end
 end
