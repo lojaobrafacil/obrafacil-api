0b8f19e6d0db750a7160e2697c523e70d6b691d8
Arthur Moura <arthurjm95@gmail.com>
2019-05-03 13:19:41 -0300

    admin

--- app/workers/deca_employees_worker.rb
+++ app/workers/deca_employees_worker.rb
@@ -2,10 +2,12 @@ class DecaEmployeesWorker
   include Sidekiq::Worker
 
   def perform(id)
-    log = Log::Worker.create(name: "DecaEmployeesWorker")
     @employee = Employee.find(id)
-    service = Deca::EmployeesService.new(id: @employee.id, query: { cpf_cnpj: @employee.federal_registration })
-    service.send
-    log.update(finished_at: Time.now, content: service.as_json, status: "OK")
+    if !@employee.admin?
+      log = Log::Worker.create(name: "DecaEmployeesWorker")
+      service = Deca::EmployeesService.new(id: @employee.id, query: { cpf_cnpj: @employee.federal_registration })
+      service.send
+      log.update(finished_at: Time.now, content: service.as_json, status: "OK")
+    end
   end
 end
