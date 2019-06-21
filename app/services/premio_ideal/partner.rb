b352908c6d135aab6f2dd7028b867db0f1336fce
Arthur Moura <arthurjm95@gmail.com>
2019-06-21 12:50:07 -0300

    adjusting body_params

--- app/services/premio_ideal/partner.rb
+++ app/services/premio_ideal/partner.rb
@@ -35,44 +35,63 @@ module PremioIdeal
     end
 
     def body_params
-      query_response = ActiveRecord::Base.connection.execute("select 
-        partners.name, 
-        COALESCE(partners.favored_federal_registration, partners.federal_registration) as cpfCnpj, 
-        COALESCE(addresses.street, 'null') as address,
-        COALESCE(addresses.number, '000') as number, 
-        COALESCE(addresses.complement, 'null') as complement, 
-        COALESCE(addresses.neighborhood, 'null') as cityRegion,
-        COALESCE(cities.name, 'null') as city, 
-        COALESCE(states.acronym, 'nd') as state, 
-        COALESCE(addresses.zipcode, '00000000') as zipcode, 
-        COALESCE(SUBSTRING(phones.phone, 4, 2), '00') as phoneDdd,
-        COALESCE(SUBSTRING(phones.phone, 6, 100), '000000000') as phoneNumber,
-        COALESCE(SUBSTRING(phones.phone, 4, 2), '00') as cellDdd,
-        COALESCE(SUBSTRING(phones.phone, 6, 100), '000000000') as cellNumber,
-        COALESCE(emails.email, 'null@null.com') as email,
-        phones.primary,
-        partners.started_date as birthDate,
-        0 as gender
-      from partners, addresses, phones, cities, states, emails
-        where partners.id = addresses.addressable_id 
-          and	partners.id = phones.phonable_id 
-          and partners.id = emails.emailable_id
-          and addresses.addressable_type = 'Partner'
-          and phones.phonable_type = 'Partner'
-          and emails.emailable_type = 'Partner'
-          and addresses.city_id = cities.id
-          and cities.state_id = states.id
-          and partners.id = #{@partner.id}
-          and emails.primary = phones.primary
-          and partners.started_date is null").as_json
-      if query_response.empty?
-      elsif resp = query_response.select { |e| e["primary"] == true }.first
-        resp.delete "primary"
-        resp
-      else
-        query_response.first.delete "primary"
-        query_response
-      end
+      # query_response = ActiveRecord::Base.connection.execute("select
+      #   partners.name,
+      #   COALESCE(partners.favored_federal_registration, partners.federal_registration) as cpfCnpj,
+      #   COALESCE(addresses.street, 'null') as address,
+      #   COALESCE(addresses.number, '000') as number,
+      #   COALESCE(addresses.complement, 'null') as complement,
+      #   COALESCE(addresses.neighborhood, 'null') as cityRegion,
+      #   COALESCE(cities.name, 'null') as city,
+      #   COALESCE(states.acronym, 'nd') as state,
+      #   COALESCE(addresses.zipcode, '00000000') as zipcode,
+      #   COALESCE(SUBSTRING(phones.phone, 4, 2), '00') as phoneDdd,
+      #   COALESCE(SUBSTRING(phones.phone, 6, 100), '000000000') as phoneNumber,
+      #   COALESCE(SUBSTRING(phones.phone, 4, 2), '00') as cellDdd,
+      #   COALESCE(SUBSTRING(phones.phone, 6, 100), '000000000') as cellNumber,
+      #   COALESCE(emails.email, 'null@null.com') as email,
+      #   phones.primary,
+      #   partners.started_date as birthDate,
+      #   0 as gender
+      # from partners, addresses, phones, cities, states, emails
+      #   where partners.id = addresses.addressable_id
+      #     and	partners.id = phones.phonable_id
+      #     and partners.id = emails.emailable_id
+      #     and addresses.addressable_type = 'Partner'
+      #     and phones.phonable_type = 'Partner'
+      #     and emails.emailable_type = 'Partner'
+      #     and addresses.city_id = cities.id
+      #     and cities.state_id = states.id
+      #     and partners.id = #{@partner.id}
+      #     and emails.primary = phones.primary
+      #     and partners.started_date is null").as_json
+      # if query_response.empty?
+      # elsif resp = query_response.select { |e| e["primary"] == true }.first
+      #   resp.delete "primary"
+      #   resp
+      # else
+      #   query_response.first.delete "primary"
+      #   query_response
+      # end
+      register = @partner.favored_federal_registration && !@partner.favored_federal_registration.empty? ? @partner.favored_federal_registration : @partner.federal_registration
+      {
+        "name": @partner.name.as_json,
+        "cpfCnpj": register,
+        "address": if @partner.addresses.empty?; "null"; elsif @partner.addresses.first.street.nil? || @partner.addresses.first.street == ""; "null"; else @partner.addresses.first.street.as_json end,
+        "number": if @partner.addresses.empty?; "000"; elsif @partner.addresses.first.number.nil? || @partner.addresses.first.number == ""; "000"; else @partner.addresses.first.number.as_json end,
+        "complement": if @partner.addresses.empty?; "null"; elsif @partner.addresses.first.complement.nil? || @partner.addresses.first.complement == ""; "null"; else @partner.addresses.first.complement.as_json end,
+        "cityRegion": if @partner.addresses.empty?; "null"; elsif @partner.addresses.first.neighborhood.nil? || @partner.addresses.first.neighborhood == ""; "null"; else @partner.addresses.first.neighborhood.as_json end,
+        "city": if @partner.addresses.empty?; "null"; elsif @partner.addresses.first.city.nil? || @partner.addresses.first.city == ""; "null"; else @partner.addresses.first.city.name.as_json end,
+        "state": if @partner.addresses.empty?; "nd"; elsif @partner.addresses.first.city.nil? || @partner.addresses.first.city == ""; "nd"; else @partner.addresses.first.city.state.acronym.as_json end,
+        "zipcode": if @partner.addresses.empty?; "00000000"; elsif @partner.addresses.first.zipcode.nil? || @partner.addresses.first.zipcode == ""; "00000000"; else @partner.addresses.first.zipcode.as_json.tr("-", "") end,
+        "phoneDdd": @partner.phones.empty? ? "00" : @partner.phones.first.phone.delete(" ").delete("-")[3..4].as_json,
+        "phoneNumber": @partner.phones.empty? ? "000000000" : @partner.phones.first.phone.delete(" ").delete("-")[5..13].as_json,
+        "cellDdd": @partner.phones.empty? ? "00" : @partner.phones.first.phone.delete(" ").delete("-")[3..4].as_json,
+        "cellNumber": @partner.phones.empty? ? "000000000" : @partner.phones.first.phone.delete(" ").delete("-")[5..13].as_json,
+        "email": @partner.primary_email.email ? @partner.primary_email.email : "null@null.com",
+        "birthDate": @partner.started_date.as_json,
+        "gender": 0,
+      }
     end
 
     def retry_by_log(log_id)
