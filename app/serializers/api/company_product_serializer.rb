47ef1b11003e52fa1160f51e2f094f63ccb29483
Arthur Moura <arthurjm95@gmail.com>
2019-08-20 10:28:35 -0300

    adding code

--- app/serializers/api/company_product_serializer.rb
+++ app/serializers/api/company_product_serializer.rb
@@ -1,5 +1,5 @@
 class Api::CompanyProductSerializer < ActiveModel::Serializer
-  attributes :id, :company_id, :company_name, :stock, :stock_min, :stock_max,
+  attributes :id, :company_id, :code, :company_name, :stock, :stock_min, :stock_max,
              :cost, :discount, :st, :margin, :updated_at, :created_at
 
   def company_name
