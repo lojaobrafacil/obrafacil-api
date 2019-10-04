cd451cdce23d4772e0993adc381d0e36cdf06981
Arthur Moura <arthurjm95@gmail.com>
2019-10-04 00:16:41 -0300

    adjusting stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,9 +4,4 @@ class Stock < ApplicationRecord
   has_many :prices
   accepts_nested_attributes_for :prices, allow_destroy: true
   validates_presence_of [:stock, :stock_max, :stock_min]
-  before_save :internal_code
-
-  def internal_code
-    self.code |= self.product.id
-  end
 end
