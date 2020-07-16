31a2d54d1824805a1399d3fc901bcb061f324e4a
Arthur Moura <arthurjm95@gmail.com>
2020-07-16 12:43:50 -0300

    adjusting commission

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -1,3 +1,16 @@
 class Commission < ApplicationRecord
   belongs_to :partner
+  before_save :set_date, on: :create
+
+  def set_date
+    time = (Time.now + 1.month).beginning_of_month
+    while [0, 6].include?(time.wday)
+      time = time + 1.day
+    end
+    time = time + 15.days
+    while [0, 6].include?(time.wday)
+      time = time + 1.day
+    end
+    self.sent_date = time
+  end
 end
