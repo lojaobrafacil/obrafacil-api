7ea39730310ee2ef4c993ed6911def32333554a8
Arthur Moura <arthurjm95@gmail.com>
2020-07-30 14:39:01 -0300

    change date

--- app/models/commission.rb
+++ app/models/commission.rb
@@ -3,14 +3,7 @@ class Commission < ApplicationRecord
   before_save :set_date, on: :create
 
   def set_date
-    time = (self.order_date + 1.month).beginning_of_month
-    while [0, 6].include?(time.wday)
-      time = time + 1.day
-    end
-    time = time + 15.days
-    while [0, 6].include?(time.wday)
-      time = time + 1.day
-    end
+    time = (self.order_date + 1.month).beginning_of_month + 13.day
     self.sent_date = time + 12.hour
   end
 end
