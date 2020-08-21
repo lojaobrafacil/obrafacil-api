d8171c4ed90b7e82f62eae72dcac8a5744593f71
Arthur Moura <arthurjm95@gmail.com>
2020-08-21 14:10:57 -0300

    adjusting imagess

--- app/serializers/api_partner/highlight_serializer.rb
+++ app/serializers/api_partner/highlight_serializer.rb
@@ -1,4 +1,8 @@
 class ApiPartner::HighlightSerializer < ActiveModel::Serializer
   attributes :id, :title, :subtitle, :image, :metadata, :starts_in, :expires_at,
              :status, :kind, :position, :link, :images, :created_at, :updated_at
+
+  def image
+    object.image.filename.nil? && object.images&.first ? object.images.first.attachment.as_json : object.image.as_json
+  end
 end
