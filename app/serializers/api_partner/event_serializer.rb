a56f56942e4becebfa0d0414d85de7f054464139
Arthur Moura <arthurjm95@gmail.com>
2020-08-24 18:48:15 -0300

    adjusitng events

--- app/serializers/api_partner/event_serializer.rb
+++ app/serializers/api_partner/event_serializer.rb
@@ -1,4 +1,8 @@
 class ApiPartner::EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :subtitle, :image, :secondaryImage, :metadata, :starts_in, :expires_at,
-             :status, :link, :created_at, :updated_at
+  attributes :id, :title, :subtitle, :image, :images, :secondaryImage, :metadata,
+             :starts_in, :expires_at, :status, :link, :created_at, :updated_at
+
+  def image
+    object.image.url.nil? && object.images&.first ? object.images.first.attachment.as_json : object.image.as_json
+  end
 end
