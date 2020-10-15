3de3104aba0c7af51a5a32142c3447e7dbc4ef85
Arthur Moura <arthurjm95@gmail.com>
2020-10-15 12:52:20 -0300

    partner serializers

--- app/serializers/api_partner/partner_serializer.rb
+++ app/serializers/api_partner/partner_serializer.rb
@@ -1,6 +1,6 @@
 class ApiPartner::PartnerSerializer < ActiveModel::Serializer
-  attributes :id, :name, :register, :site, :addresses, :phones,
-             :emails, :avatar
+  attributes :id, :name, :register, :aboutme, :site, :addresses, :phones,
+             :emails, :avatar, :instagram, :phone, :email, :address
 
   def phones
     object.phones.order(primary: :desc).map { |u| ActiveModelSerializers::Adapter.configured_adapter.new(ApiPartner::PhoneSerializer.new(u)).serializable_hash }
@@ -12,5 +12,17 @@ class ApiPartner::PartnerSerializer < ActiveModel::Serializer
 
   def addresses
     object.addresses.map { |u| ActiveModelSerializers::Adapter.configured_adapter.new(ApiPartner::AddressSerializer.new(u)).serializable_hash }
+  end
+
+  def phone
+    phones[0]
+  end
+
+  def email
+    emails[0]
+  end
+
+  def address
+    addresses[0]
   end
 end
--- app/serializers/api_partner/partners_serializer.rb
+++ app/serializers/api_partner/partners_serializer.rb
@@ -2,12 +2,12 @@ class ApiPartner::PartnersSerializer < ActiveModel::Serializer
   attributes :id, :name, :register, :site, :address, :phone,
              :email, :avatar, :projects, :aboutme, :instagram
 
-  def phone
-    ActiveModelSerializers::Adapter.configured_adapter.new(ApiPartner::PhoneSerializer.new(object.primary_phone)).serializable_hash
-  end
-
   def projects
     object.projects.order(project_date: :desc).limit(4).map { |u| ActiveModelSerializers::Adapter.configured_adapter.new(ApiPartner::PartnerProjectSerializer.new(u)).serializable_hash } rescue []
+  end
+
+  def phone
+    ActiveModelSerializers::Adapter.configured_adapter.new(ApiPartner::PhoneSerializer.new(object.primary_phone)).serializable_hash
   end
 
   def email
