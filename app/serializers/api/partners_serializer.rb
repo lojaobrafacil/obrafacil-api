077c05d830a407d25803b3a87c017d5781f2b537
Arthur Moura <arthurjm95@gmail.com>
2022-05-19 09:38:41 -0300

    added partner group to return index partners

--- app/serializers/api/partners_serializer.rb
+++ app/serializers/api/partners_serializer.rb
@@ -1,4 +1,4 @@
 class Api::PartnersSerializer < ActiveModel::Serializer
   attributes :id, :name, :federal_registration, :state_registration, :status,
-             :description, :cash_redemption, :created_at
+             :description, :cash_redemption, :partner_group, :created_at
 end
